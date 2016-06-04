bc78f5cc651d671b9ecbcf578918cc087f08de7d
David Arroyo <droyo@aqwari.net>
2016-06-03 20:20:12 -0400

    Add examples for marshal/unmarshal

--- xmltree/example_test.go
+++ xmltree/example_test.go
@@ -117,3 +117,110 @@ func ExampleElement_SearchFunc() {
 	// michaelp@work.com
 	// michael.thompson@work.com
 }
+
+func ExampleUnmarshal() {
+	var input = []byte(`<mediawiki xml:lang="en">
+	  <page>
+	    <title>Page title</title>
+	    <restrictions>edit=sysop:move=sysop</restrictions>
+	    <revision>
+	      <timestamp>2001-01-15T13:15:00Z</timestamp>
+	      <contributor><username>Foobar</username></contributor>
+	      <comment>I have just one thing to say!</comment>
+	      <text>A bunch of [[text]] here.</text>
+	      <minor />
+	    </revision>
+	    <revision>
+	      <timestamp>2001-01-15T13:10:27Z</timestamp>
+	      <contributor><ip>10.0.0.2</ip></contributor>
+	      <comment>new!</comment>
+	      <text>An earlier [[revision]].</text>
+	    </revision>
+	  </page>
+	  
+	  <page>
+	    <title>Talk:Page title</title>
+	    <revision>
+	      <timestamp>2001-01-15T14:03:00Z</timestamp>
+	      <contributor><ip>10.0.0.2</ip></contributor>
+	      <comment>hey</comment>
+	      <text>WHYD YOU LOCK PAGE??!!! i was editing that jerk</text>
+	    </revision>
+	  </page>
+	</mediawiki>`)
+
+	type revision struct {
+		Timestamp   string   `xml:"timestamp"`
+		Contributor string   `xml:"contributor>ip"`
+		Comment     string   `xml:"comment"`
+		Text        []string `xml:"text"`
+	}
+
+	root, err := xmltree.Parse(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Pull all <revision> items from the input
+	for _, el := range root.Search("", "revision") {
+		var rev revision
+		if err := xmltree.Unmarshal(el, &rev); err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(rev.Timestamp, rev.Comment)
+	}
+
+	// Output:
+	// 2001-01-15T13:15:00Z I have just one thing to say!
+	// 2001-01-15T13:10:27Z new!
+	// 2001-01-15T14:03:00Z hey
+}
+
+func ExampleMarshal() {
+	var input = []byte(`<?xml version="1.0" encoding="UTF-8"?>
+	<toc>
+	  <chapter-list>
+	    <chapter>
+	      <title>Civilizing Huck.Miss Watson.Tom Sawyer Waits.</title>
+	      <number>1</number>
+	    </chapter>
+	    <chapter>
+	      <title>The Boys Escape Jim.Torn Sawyer's Gang.Deep-laid Plans.</title>
+	      <number>2</number>
+	    </chapter>
+	    <chapter>
+	      <title>A Good Going-over.Grace Triumphant."One of Tom Sawyers's Lies".</title>
+	      <number>3</number>
+	    </chapter>
+	    <chapter>
+	      <title>Huck and the Judge.Superstition.</title>
+	      <number>4</number>
+	    </chapter>
+	  </chapter-list>
+	</toc>`)
+
+	var chapters []xmltree.Element
+	root, err := xmltree.Parse(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, el := range root.Search("", "chapter") {
+		for _, child := range el.Search("", "title") {
+			el.Content = child.Content
+		}
+		el.Children = nil
+		chapters = append(chapters, *el)
+	}
+	root.Children = chapters
+	fmt.Printf("%s\n", xmltree.MarshalIndent(root, "", "  "))
+
+	// Output:
+	// <toc>
+	//   <chapter>Civilizing Huck.Miss Watson.Tom Sawyer Waits.</chapter>
+	//   <chapter>The Boys Escape Jim.Torn Sawyer's Gang.Deep-laid Plans.</chapter>
+	//   <chapter>A Good Going-over.Grace Triumphant."One of Tom Sawyers's Lies".</chapter>
+	//   <chapter>Huck and the Judge.Superstition.</chapter>
+	// </toc>
+}
